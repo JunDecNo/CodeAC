2c7192d6e163e06f50a35929b34977b3e18d0d7e
JunDecNo <jundecno@gmail.com>
2023-02-15 22:53:53 +0800

    2023215

--- LC1-500/110.平衡二叉树.cpp
+++ LC1-500/110.平衡二叉树.cpp
@@ -1,6 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-/** * Definition for a binary tree node. * struct TreeNode { *     int val; *     TreeNode *left; *     TreeNode *right; *     TreeNode() : val(0), left(nullptr), right(nullptr) {} *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} * }; */class Solution {public:    bool isBalanced(TreeNode* root) {    }};
-int main(){
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        // 左右子树高度差大于一则不是
+        if(root==NULL)return true;
+        if(hight(root->left)>hight(root->right)+1)return false;
+        else if(hight(root->right)>hight(root->left)+1)return false;
+        else return isBalanced(root->left)&&isBalanced(root->right);
+    }
+    int hight(TreeNode *root){
+        if(root==NULL)return 0;
+        return max(hight(root->left),hight(root->right))+1;
+    }
+};
+int main() {
     return 0;
 }
--- LGP4001-4500/P4326[COCI2006-2007#1]Herman.cpp
+++ LGP4001-4500/P4326[COCI2006-2007#1]Herman.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+#include <iomanip>
+#define pi 3.14159265358979323
 using namespace std;
 int main(){
+    double r;
+    cin>>r;
+    cout<<fixed<<setprecision(6)<<(pi*r*r)<<endl;
+    cout<<fixed<<setprecision(6)<<(2*r*r)<<endl;
     return 0;
 }
