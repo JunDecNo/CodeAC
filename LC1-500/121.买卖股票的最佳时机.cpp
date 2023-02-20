4c5a41ca25ae3bd85ba0bacc3aacc97c8eec7858
JunDecNo <jundecno@gmail.com>
2023-02-20 17:18:15 +0800

    23220

--- LC1-500/121.买卖股票的最佳时机.cpp
+++ LC1-500/121.买卖股票的最佳时机.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-class Solution {public:    int maxProfit(vector<int>& prices) {    }};
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int pmin=prices[0],pmax=0;
+        for(int i=0;i<prices.size();i++){
+            pmax=max(pmax,prices[i]-pmin);
+            pmin=min(pmin,prices[i]);
+        }
+        return pmax;
+    }
+};
 int main(){
     return 0;
 }
--- LGP4001-4500/P4445[AHOI2018初中组]报名签到.cpp
+++ LGP4001-4500/P4445[AHOI2018初中组]报名签到.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int len;
+    long int sum=0;
+    cin>>len;
+    int *arr=new int[len];
+    for(int i=0;i<len;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<len-1;i++){
+        sum+=max(arr[i],arr[i+1]);
+    }
+    cout<<sum<<endl;
     return 0;
 }
