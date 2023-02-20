680ce0cc8f0f183afafd4e4de5932f18d98eab05
JunDecNo <jundecno@gmail.com>
2023-02-21 02:31:43 +0800

    23221

--- LC1-500/125.验证回文串.cpp
+++ LC1-500/125.验证回文串.cpp
@@ -1,6 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Solution {public:    bool isPalindrome(string s) {    }};
-int main(){
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int f=0,e=s.size()-1;f<=e; ){
+            char front=s[f],end=s[e];
+            if(isLetter(front)&&isLetter(end)){
+                if(BeLittle(front)==BeLittle(end)){
+                    f++;e--;
+                }else return false;
+            }
+            else if(!isLetter(front))f++;
+            else if(!isLetter(end))e--;
+        }
+        return true;
+    }
+    bool isLetter(char ch){
+        if((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')||(ch>='0'&&ch<='9'))return true;
+        else return false;
+    }
+    char BeLittle(char ch){
+        if(ch>='A'&&ch<='Z')return (char)(ch+32);
+        else return ch;
+    }
+};
+int main() {
     return 0;
 }
--- LGP4501-5000/P4613[COCI2017-2018#5]Olivander.cpp
+++ LGP4501-5000/P4613[COCI2017-2018#5]Olivander.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
+    int len;
+    cin>>len;
+    int *a=new int[len],*b=new int[len];
+    for(int i =0;i<len;i++)cin>>a[i];
+    for(int i =0;i<len;i++)cin>>b[i];
+    sort(a,a+len);
+    sort(b,b+len);
+    bool res=true;
+    for(int i =0;i<len;i++){
+        if(a[i]>b[i]){
+            res=false;
+            break;
+        }
+    }
+    if(res)cout<<"DA";
+    else cout<<"NE";
     return 0;
 }
