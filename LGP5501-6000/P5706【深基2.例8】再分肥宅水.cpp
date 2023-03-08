774f85d2ab5d32d1ae9fd1258a35f334c20536be
JunDecNo <jundecno@gmail.com>
2023-03-08 23:17:13 +0800

    230308

--- LGP5501-6000/P5706【深基2.例8】再分肥宅水.cpp
+++ LGP5501-6000/P5706【深基2.例8】再分肥宅水.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main(){
+    double t;
+    int n;
+    cin>>t>>n;
+    cout<<fixed<<setprecision(3)<<(t/n)<<endl;
+    cout<<2*n<<endl;
     return 0;
 }
