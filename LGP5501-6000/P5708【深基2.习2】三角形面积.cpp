004a2c2410b5184cf6a0070cac06fd70c7ed97fb
JunDecNo <jundecno@gmail.com>
2023-03-09 22:51:23 +0800

    2339

--- LGP5501-6000/P5708【深基2.习2】三角形面积.cpp
+++ LGP5501-6000/P5708【深基2.习2】三角形面积.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+#include<iomanip>
+#include<cmath>
 using namespace std;
 int main(){
+    double a,b,c;
+    cin>>a>>b>>c;
+    double p = (a+b+c)/2;
+    cout<<fixed<<setprecision(1)<<sqrt(p*(p-a)*(p-b)*(p-c));
     return 0;
 }
--- LGP5501-6000/P5709【深基2.习6】ApplesPrologue_苹果和虫子.cpp
+++ LGP5501-6000/P5709【深基2.习6】ApplesPrologue_苹果和虫子.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int m,t,s;
+    cin>>m>>t>>s;
+    if(!t)cout<<0<<endl;
+    else{
+        int x=s/t+(s%t!=0);
+        cout<<max(m-x,0)<<endl;
+    }
     return 0;
 }
