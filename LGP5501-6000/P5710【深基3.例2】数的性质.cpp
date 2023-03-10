8aaa4f33a2591dd01ef1fceb2353716316506941
JunDecNo <jundecno@gmail.com>
2023-03-10 23:45:52 +0800

    23310

--- LGP5501-6000/P5710【深基3.例2】数的性质.cpp
+++ LGP5501-6000/P5710【深基3.例2】数的性质.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int x;
+    cin>>x;
+    bool one=(x%2==0),two=(x>4&&x<=12);
+    cout<<(one&&two)<<" "<<(one||two)<<" "<<((!one&&two)||(one&&!two))<<" "<<(!one&&!two)<<endl;
     return 0;
 }
