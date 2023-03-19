25e413cc61ac9a9d26def61d923c911bbb2d6df4
JunDecNo <jundecno@gmail.com>
2023-03-19 23:25:36 +0800

    23319

--- LGP5501-6000/P5724【深基4.习5】求极差_最大跨度值.cpp
+++ LGP5501-6000/P5724【深基4.习5】求极差_最大跨度值.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n,tmp;
+    cin>>n;
+    cin>>tmp;
+    int max=tmp,min=tmp;
+    for(int i=1;i<n;i++){
+        cin>>tmp;
+        if(max<tmp)max=tmp;
+        else if(min>tmp)min=tmp;
+    }
+    cout<<max-min<<endl;
     return 0;
 }
--- LGP5501-6000/P5726【深基4.习9】打分.cpp
+++ LGP5501-6000/P5726【深基4.习9】打分.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std;
+int func(int x){
+    if(x==1){
+        cout<<1<<" ";
+        return 1;
+    }
+    else if(x%2==0){
+        func(x/2);
+    }
+    else{
+        func(x*3+1);
+    }   
+    cout<<x<<" ";
+}
 int main(){
+    int x;
+    cin>>x;
+    func(x);
     return 0;
 }
