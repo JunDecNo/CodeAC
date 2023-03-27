baeda782f078935a0d5b9c9c63beb97ddac507e5
JunDecNo <jundecno@gmail.com>
2023-03-27 15:13:09 +0800

    23327

--- LGP5501-6000/P5728【深基5.例5】旗鼓相当的对手.cpp
+++ LGP5501-6000/P5728【深基5.例5】旗鼓相当的对手.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+int a[1001][3];
 int main(){
+    int n,cnt=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i][0];
+        cin>>a[i][1];
+        cin>>a[i][2];
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(abs(a[i][0]-a[j][0])<=5&&abs(a[i][1]-a[j][1])<=5&&abs(a[i][2]-a[j][2])<=5){
+                if(abs(a[i][0]+a[i][1]+a[i][2]-a[j][0]-a[j][1]-a[j][2])<=10)cnt++;
+            }
+        }
+    }
+    cout<<cnt;
     return 0;
 }
--- LGP5501-6000/P5729【深基5.例7】工艺品制作.cpp
+++ LGP5501-6000/P5729【深基5.例7】工艺品制作.cpp
@@ -1,5 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int arr[21][21][21] = {0};
+int main() {
+    int w, x, h, q;
+    int x1, x2, x3, y1, y2, y3;
+    int cnt=0;
+    cin >> w >> x >> h;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> x1 >> x2 >> x3 >> y1 >> y2 >> y3;
+        for (int a = x1; a <= y1; a++) {
+            for (int b = x2; b <= y2; b++) {
+                for (int c = x3; c <= y3; c++) {
+                    arr[a][b][c] = 1;
+                }
+            }
+        }
+    }
+    for (int a = 1; a <= w; a++) {
+        for (int b = 1; b <= x; b++) {
+            for (int c = 1; c <= h; c++) {
+                if(arr[a][b][c]==0)cnt++;
+            }
+        }
+    }
+    cout<<cnt;
     return 0;
 }
